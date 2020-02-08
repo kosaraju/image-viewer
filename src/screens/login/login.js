7c80b217391ead99b37258634deedfabf0653b6a
kosaraju <gopala.kosaraju@ing.nl>
2020-02-08 13:40:28 +0100

    Added Login Component with Card FormControl and Login buttons

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,13 +1,41 @@
 import React, {Component} from "react";
 import "./Login.css";
 import Header from '../../common/Header/Header'
+import {
+    Card,
+    CardActions,
+    Typography,
+    Button,
+    CardContent,
+    InputLabel, FormControl, Input, FormHelperText
+} from '@material-ui/core';
+
 
 class Login extends Component{
     render(){
         return(
             <div>
                 <Header/>
-                
+                <Card className='loginCard' variant="outlined">
+                    <CardContent className='loginCardContent'>
+                        <Typography variant="h5" gutterBottom>
+                            LOGIN
+                        </Typography>
+                        <br/>
+                        <FormControl required>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input id="username" type="text" />
+                        </FormControl>
+                        <br/><br/>
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password" />
+                        </FormControl>
+                        <br/><br/>
+                        <Button id='loginButton' variant="contained" color="primary">LOGIN</Button>
+                    </CardContent>
+
+                </Card>
             </div>
         )
     }
